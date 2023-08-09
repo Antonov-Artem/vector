52cdc86b46a872521ec431878ad40ef49e32536e
Artem Antonov <onlyformywork0@gmail.com>
2023-08-09 12:30:41 -0400

    :sparkles: Core functionality
    Added vector64 data type and constructor

--- vector.go
+++ vector.go
@@ -1,1 +1,26 @@
+/*
+# Vector
+
+This is a simple package for working with 3D vectors
+*/
 package vector
+
+/*
+Represents 3D vector data type
+*/
+type vector64 struct {
+	X float64
+	Y float64
+	Z float64
+}
+
+/*
+Initializes 3D vector
+*/
+func Vector(x float64, y float64, z float64) vector64 {
+	return vector64{
+		X: x,
+		Y: y,
+		Z: z,
+	}
+}
